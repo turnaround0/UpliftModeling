ed941e509919e0381c40d81a0ecdbaf27e0d29ef
Jaehwan Park <turnaround0@gmail.com>
2019-08-27 16:56:14 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 
 from models import model_tma, model_dta, model_lai, model_glai, model_rvtu, model_rf
 
 models = {
     'tma': model_tma,
-    'dta': model_dta,
-    'lai': model_lai,
-    'glai': model_glai,
-    'trans': model_rvtu,
-    'urf_ed': model_rf,
-    'urf_kl': model_rf,
-    'urf_chisq': model_rf,
-    'urf_int': model_rf,
+    #'dta': model_dta,
+    #'lai': model_lai,
+    #'glai': model_glai,
+    #'trans': model_rvtu,
+    #'urf_ed': model_rf,
+    #'urf_kl': model_rf,
+    #'urf_chisq': model_rf,
+    #'urf_int': model_rf,
 }
 search_space = {
     'method': [LogisticRegression],
@@ -19,7 +19,10 @@ search_space = {
     'penalty': ['none', 'l2'],
     'tol': [1e-2, 1e-3, 1e-4],
     'C': [1e6, 1e3, 1, 1e-3, 1e-6],
-    'max_iter': [10000],
+    'max_iter': [100],
+}
+search_space_for_linear = {
+    'method': [LinearRegression],
 }
 search_space_for_tree = {
     'ntree': [10, 20, ],
@@ -44,9 +47,12 @@ urf_methods = {
 wrapper_models = ['tma', 'dta', 'trans']
 
 
-def get_search_space(model_name):
+def get_search_space(dataset_name, model_name):
     if model_name in ['tma', 'trans', 'lai', 'glai']:
-        return search_space
+        if dataset_name == 'lalonde':
+            return search_space_for_linear
+        else:
+            return search_space
     elif model_name == 'dta':
         return search_space_for_dta
     elif model_name.startswith('urf'):
