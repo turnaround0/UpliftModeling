00104f1f77bbf1859ce63d48f04afb9c0464f617
Jaehwan Park <turnaround0@gmail.com>
2019-09-02 22:26:35 +0900

    Update config file

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 
 from models import model_tma, model_dta, model_lai, model_glai, model_rvtu, model_rf
 
-models = {
+hillstrom_models = {
     'tma': model_tma,
     'dta': model_dta,
     'lai': model_lai,
@@ -19,6 +19,16 @@ lalonde_models = {
     'urf_ed': model_rf,
     'urf_kl': model_rf,
     'urf_chisq': model_rf,
+}
+criteo_models = {
+    'tma': model_tma,
+    'dta': model_dta,
+    'lai': model_lai,
+    'glai': model_glai,
+    'trans': model_rvtu,
+    'urf_ed': model_rf,
+    'urf_kl': model_rf,
+    'urf_chisq': model_rf,
     'urf_int': model_rf,
 }
 search_space = {
@@ -31,7 +41,7 @@ search_space = {
 search_space_for_linear = {
     'method': [LinearRegression],
 }
-search_space_for_tree = {
+search_space_for_tree_hillstrom = {
     'ntree': [10, ],
     # 'mtry': [3, ],  default = sqrt(#col)
     'bagging_fraction': [0.6, ],
@@ -64,9 +74,9 @@ wrapper_models = ['tma', 'dta', 'trans']
 
 def get_models(dataset_name):
     return {
-        'hillstrom': models,
+        'hillstrom': hillstrom_models,
         'lalonde': lalonde_models,
-        'criteo': models,
+        'criteo': criteo_models,
     }[dataset_name]
 
 
@@ -82,6 +92,6 @@ def get_search_space(dataset_name, model_name):
         elif dataset_name == 'criteo':
             return search_space_for_tree_criteo
         else:
-            return search_space_for_tree
+            return search_space_for_tree_hillstrom
     else:
         return {}
