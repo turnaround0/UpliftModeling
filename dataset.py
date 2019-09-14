0a2a1ffe72dc33594d5f260d28d5fe9d1f889b00
Jaehwan Park <turnaround0@gmail.com>
2019-09-14 17:45:19 +0900

    Move input/output folders

--- dataset.py
+++ dataset.py
@@ -8,22 +8,22 @@ def load_data(dataset_name):
     # Link of lalonde: https://drive.google.com/open?id=1b8N7WtwIe2WmQJD1KL5UAy70K13MxwKj
     # Link of criteo: https://drive.google.com/open?id=1Vxv7JiEyFr2A99xT6vYzB5ps5WhvV7NE
     if dataset_name == 'hillstrom':
-        return pd.read_csv('Hillstrom.csv')
+        return pd.read_csv('input/Hillstrom.csv')
     elif dataset_name == 'lalonde':
-        return pd.read_csv('Lalonde.csv')
+        return pd.read_csv('input/Lalonde.csv')
     elif dataset_name == 'criteo':
-        return pd.read_csv('criteo_small_fix.csv')
+        return pd.read_csv('input/criteo_small_fix.csv')
     else:
         return None
 
 
 def save_json(name, data):
-    with open(name + '.json', 'w') as f:
+    with open('output/' + name + '.json', 'w') as f:
         json.dump(data, f)
 
 
 def load_json(name):
-    with open(name + '.json', 'r') as f:
+    with open('output/' + name + '.json', 'r') as f:
         print('Open success')
         return json.load(f)
 
