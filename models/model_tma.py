06882a00693d71c6e814fb28c3b9d0d98a38e679
Jaehwan Park <turnaround0@gmail.com>
2019-09-17 11:34:24 +0900

    Update model_tma.py

--- models/model_tma.py
+++ models/model_tma.py
@@ -1,82 +1,112 @@
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression, LogisticRegression
 
+ext_params = {}
+
+
+def set_params(max_round, u_list, final_uplift):
+    global ext_params
+
+    ext_params = {
+        'max_round': max_round,
+        'u_list': final_uplift + np.abs(final_uplift) * np.array(u_list),
+    }
+    print('Extraction params:', ext_params)
+
+
+def get_predict_values(model_fit, data):
+    if isinstance(model_fit[0], LinearRegression):
+        pred_treat = model_fit[0].predict(data)
+    else:
+        pred_treat = model_fit[0].predict_proba(data)[:, 1]
+
+    if isinstance(model_fit[1], LinearRegression):
+        pred_control = model_fit[1].predict(data)
+    else:
+        pred_control = model_fit[1].predict_proba(data)[:, 1]
+
+    return pred_treat, pred_control
+
 
 def fit(x, y, t, method=LogisticRegression, **kwargs):
-    """Training a model according to the "Two Model Approach"
-    (a.k.a. "Separate Model Approach")
-    The default model is General Linear Model (GLM)
+    fit_list = []
+    rest = len(y)
 
-    Source: "Incremental Value Modeling" (Hansotia, 2002)
+    full_x = x.copy().reset_index(drop=True)
+    full_y = y.copy().reset_index(drop=True)
+    full_t = t.copy().reset_index(drop=True)
+    x, y, t = full_x, full_y, full_t
 
-    Args:
-        x: A data frame of predictors.
-        y: A binary response (numeric) vector.
-        t: A binary response (numeric) representing the treatment assignment
-            (coded as 0/1).
-        method: A sklearn model specifying which classification or regression
-            model to use. This should be a method that can handle a
-            multinominal class variable.
+    for idx in range(ext_params['max_round']):
+        u_value = ext_params['u_list'][idx]
+        if idx == ext_params['max_round'] - 1:
+            treat_rows = (full_t == 1)
+            control_rows = (full_t == 0)
 
-    Return:
-        Dictionary: A dictionary of two models. One for the treatment group,
-            one for the control group.
+            model_treat = method(**kwargs).fit(full_x[treat_rows], full_y[treat_rows])
+            model_control = method(**kwargs).fit(full_x[control_rows], full_y[control_rows])
 
-            {
-                'model_treat': a model for the treatment group,
-                'model_control': a model for the control group
-            }
+            fit_list.append((model_treat, model_control))
 
-    """
+            ext_idx_list = full_y.index.tolist()
+        else:
+            treat_rows = (t == 1)
+            control_rows = (t == 0)
 
-    treat_rows = (t == 1)
-    control_rows = (t == 0)
-    model_treat = method(**kwargs).fit(x[treat_rows], y[treat_rows])
-    model_control = method(**kwargs).fit(x[control_rows], y[control_rows])
+            model_treat = method(**kwargs).fit(x[treat_rows], y[treat_rows])
+            model_control = method(**kwargs).fit(x[control_rows], y[control_rows])
 
-    res = {
-        'model_treat': model_treat,
-        'model_control': model_control,
-    }
-    return res
+            pred_treat, pred_control = get_predict_values((model_treat, model_control), x)
+            uplift = pred_treat - pred_control
+            meet = pd.Series(uplift > u_value)
+            ext_idx_list = meet[meet].index.tolist()
+
+            x = x.drop(ext_idx_list).reset_index(drop=True)
+            y = y.drop(ext_idx_list).reset_index(drop=True)
+            t = t.drop(ext_idx_list).reset_index(drop=True)
+
+            fit_list.append((model_treat, model_control))
+            rest -= len(ext_idx_list)
+
+        print('Round, rest, number of extraction:', idx, rest, len(ext_idx_list))
+
+    return fit_list
 
 
 def predict(obj, newdata, **kwargs):
-    """Predictions according to the "Two Model Approach"
-    (a.k.a. "Separate Model Approach")
+    meet_list = []
+    final_pred_treat, final_pred_control = None, None
+    rest = len(newdata)
+    for idx, model_fit in enumerate(obj):
+        u_value = ext_params['u_list'][idx]
 
-    For each instance in newdata two predictions are made:
-    1) What is the probability of a person responding when treated?
-    2) What is the probability of a person responding when not treated
-      (i.e. part of control group)?
+        pred_treat, pred_control = get_predict_values(model_fit, newdata)
+        meet = pd.Series(pred_treat - pred_control > u_value)
 
-    Source: "Incremental Value Modeling" (Hansotia, 2002)
+        pred_treat = pd.Series(pred_treat)
+        pred_control = pd.Series(pred_control)
 
-    Args:
-        obj: A dictionary of two models.
-            One for the treatment group, one for the control group.
-        newdata: A data frame containing the values at which predictions
-            are required.
+        if idx == 0:
+            final_pred_treat = pred_treat
+            final_pred_control = pred_control
+            final_pred_treat[~meet] = None
+            final_pred_control[~meet] = None
+        else:
+            for prev_idx in range(idx):
+                prev_meet = meet_list[prev_idx]
+                meet[prev_meet] = False
+            final_pred_treat[meet] = pred_treat[meet]
+            final_pred_control[meet] = pred_control[meet]
 
-    Return:
-        DataFrame: A dataframe with predicted returns for when the customers
-            are treated and for when they are not treated.
-    """
+        meet_list.append(meet)
+        if idx == ext_params['max_round'] - 1:
+            rest = 0
+        else:
+            rest -= meet.sum()
+        print('Round, rest, meet count:', idx, rest, meet.sum())
 
-    if isinstance(obj['model_treat'], LinearRegression):
-        pred_treat = obj['model_treat'].predict(newdata)
-    else:
-        pred_treat = obj['model_treat'].predict_proba(newdata)[:, 1]
-
-    if isinstance(obj['model_control'], LinearRegression):
-        pred_control = obj['model_control'].predict(newdata)
-    else:
-        pred_control = obj['model_control'].predict_proba(newdata)[:, 1]
-
-    # pred_treat = obj['model_treat'].predict(newdata)
-    # pred_control = obj['model_control'].predict(newdata)
-    pred_df = pd.DataFrame({
-        "pr_y1_t1": pred_treat,
-        "pr_y1_t0": pred_control,
-    })
-    return pred_df
+    return {
+        'pr_y1_t1': final_pred_treat,
+        'pr_y1_t0': final_pred_control,
+    }
