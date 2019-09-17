12301158e7e05dcfe48d1acce956f5b6e74cad34
Jaehwan Park <turnaround0@gmail.com>
2019-09-17 09:00:45 +0900

    Update ext.py

--- config/ext.py
+++ config/ext.py
@@ -25,7 +25,7 @@ config = {
         'hillstrom': {
             'dt_ed': {'model': model_dt_ed, 'params': params_tree_hillstrom},
             'dt_ed_ext': {'model': model_dt_ed_ext, 'params': params_tree_hillstrom,
-                          'max_round': 6, 'u_list': [1.5, 1.0, 0.7, 0.5, 0.3, -float('INF')]},
+                          'max_round': 4, 'u_list': [2.0, 1.0, 0.5, -float('INF')]},
             'tma': {},
             'tma_ext': {'model': model_tma_ext, 'max_round': 3, 'u_list': [1.0, 0.7, -float('INF')]},
         },
