5b1dd90373c9527809cd0e8c928d52459eed9d69
granrookey <granrookey@gmail.com>
2019-09-20 11:49:36 +0900

    Update mlai.py

--- config/mlai.py
+++ config/mlai.py
@@ -1,15 +1,21 @@
-from models import model_mlai
+from models import model_glai
 
 config = {
     'dataset': {
         'hillstrom': {
             'glai': {},
-            'mlai': {'model': model_mlai, 'mlai_values': [0, 0.3, 0.5, 0.7, 1]},
+            # 'mlai': {'model': model_mlai, 'mlai_values': [0, 0.3, 0.5, 0.7, 1], 'class_weight':'calculate'},
+            'mlai_ed': {'model': model_glai, 'class_weight':'calculate'},
+            # 'mlai_balanced': {'model': model_mlai, 'mlai_values': [0, 0.3, 0.5, 0.7, 1], 'class_weight':'balanced'},
+            'mlai_balanced': {'model': model_glai,  'class_weight':'balanced'},
         },
         'criteo': {
             'glai': {},
-            'mlai': {'model': model_mlai, 'mlai_values': [0, 0.3, 0.5, 0.7, 1]},
-        },
+             # 'mlai': {'model': model_mlai, 'mlai_values': [0, 0.3, 0.5, 0.7, 1], 'class_weight':'calculate'},
+            'mlai_ed': {'model': model_glai, 'class_weight':'calculate'},
+            # 'mlai_balanced': {'model': model_mlai, 'mlai_values': [0, 0.3, 0.5, 0.7, 1], 'class_weight':'balanced'},
+            'mlai_balanced': {'model': model_glai,  'class_weight':'balanced'},
+        }
     },
     'wrapper': False,
     'niv': True,
