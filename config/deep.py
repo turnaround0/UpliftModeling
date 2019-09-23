6cd74f7aedc06e7148d2d1c1822cf4e25716aaf8
Jaehwan Park <turnaround0@gmail.com>
2019-09-23 15:40:44 +0900

    Update deep.py

--- config/deep.py
+++ config/deep.py
@@ -1,21 +1,33 @@
-from models import model_dta_deep
+from models import model_dta_deep, model_deep
 
 config = {
     'dataset': {
         'hillstrom': {
             'dta': {},
+            'deep': {'model': model_dta_deep,
+                     'params': {'method': 'logistic', 'lr': 1e-5, 'epochs': 50,
+                                'batch_size': 64, 'decay': 0.01}},
             'dta_deep': {'model': model_dta_deep,
-                         'params': {'method': 'logistic', 'lr': 1e-5, 'epochs': 200, 'batch_size': 64}},
+                         'params': {'method': 'logistic', 'lr': 1e-5, 'epochs': 50,
+                                    'batch_size': 64, 'decay': 0.01}},
         },
         'lalonde': {
              'dta': {},
+             'deep': {'model': model_deep,
+                      'params': {'method': 'linear', 'lr': 1e-5, 'epochs': 300,
+                                 'batch_size': 64, 'decay': 0.01}},
              'dta_deep': {'model': model_dta_deep,
-                          'params': {'method': 'linear', 'lr': 1e-3, 'epochs': 500, 'batch_size': 64}},
+                          'params': {'method': 'linear', 'lr': 1e-5, 'epochs': 300,
+                                     'batch_size': 64, 'decay': 0.01}},
         },
         'criteo': {
             'dta': {},
+            'deep': {'model': model_dta_deep,
+                     'params': {'method': 'logistic', 'lr': 1e-5, 'epochs': 100,
+                                'batch_size': 64, 'decay': 0.01}},
             'dta_deep': {'model': model_dta_deep,
-                         'params': {'method': 'logistic', 'lr': 1e-5, 'epochs': 500, 'batch_size': 64}},
+                         'params': {'method': 'logistic', 'lr': 1e-5, 'epochs': 100,
+                                    'batch_size': 64, 'decay': 0.01}},
         },
     },
     'wrapper': False,
