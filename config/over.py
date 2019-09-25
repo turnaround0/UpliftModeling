838d49f5fe3146ded42d5f6b2f096069d7810c05
Jaehwan Park <turnaround0@gmail.com>
2019-09-25 16:41:57 +0900

    Update over.py

--- config/over.py
+++ config/over.py
@@ -1,6 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 
-from over import smote, gan
+from over import smote, gan, gan2
 
 params_logistic = {
     'method': LogisticRegression,
@@ -11,11 +11,11 @@ params_gan_hillstrom = {
     'gen_lr': 1e-4,
     'dis_lr': 7e-6,
     'batch_size': 256,
-    'epochs': 50,
+    'epochs': 70,
     'noise_size': 64,
     'beta1': 0.5,
-    'major_multiple': 1,
-    'minor_ratio': 0.3,
+    'major_multiple': 1.5,
+    'minor_ratio': 0.5,
 }
 params_gan_criteo = {
     'gen_lr': 1e-4,
@@ -23,23 +23,25 @@ params_gan_criteo = {
     'batch_size': 64,
     'epochs': 100,
     'noise_size': 64,
-    'major_multiple': 2,
-    'minor_ratio': 1,
+    'major_multiple': 1.5,
+    'minor_ratio': 0.5,
 }
 
 config = {
     'dataset': {
-        'hillstrom': {
-            'dta': {'params': params_logistic},
-            'dta_smote': {'over_sampling': smote.over_sampling, 'params': params_logistic},
-            'dta_gan': {'over_sampling': gan.over_sampling, 'params': params_logistic,
-                        'params_over': params_gan_hillstrom},
-        },
+        # 'hillstrom': {
+        #     'dta': {'params': params_logistic},
+        #     'dta_smote': {'over_sampling': smote.over_sampling, 'params': params_logistic},
+        #     'dta_gan': {'over_sampling': gan.over_sampling, 'params': params_logistic,
+        #                 'params_over': params_gan_hillstrom},
+        # },
         'criteo': {
-            'dta': {'params': params_logistic},
-            'dta_smote': {'over_sampling': smote.over_sampling, 'params': params_logistic},
-            'dta_gan': {'over_sampling': gan.over_sampling, 'params': params_logistic,
-                        'params_over': params_gan_criteo},
+            # 'dta': {'params': params_logistic},
+            # 'dta_smote': {'over_sampling': smote.over_sampling, 'params': params_logistic},
+            # 'dta_gan': {'over_sampling': gan.over_sampling, 'params': params_logistic,
+            #             'params_over': params_gan_criteo},
+            'dta_gan2': {'over_sampling': gan2.over_sampling, 'params': params_logistic,
+                         'params_over': params_gan_criteo},
         },
     },
     'wrapper': False,
