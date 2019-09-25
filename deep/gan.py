47baa5fdc8ac12bc8ede87eca52cbe9b3f3a93ad
Jaehwan Park <turnaround0@gmail.com>
2019-09-25 16:51:08 +0900

    Update gan.py

--- deep/gan.py
+++ deep/gan.py
@@ -1,138 +1,117 @@
-import tensorflow as tf
+import numpy as np
 from tensorflow import keras
 
-tfgan = tf.contrib.gan
+
+def build_generator(data_dim, latent_dim):
+    model = keras.Sequential([
+        keras.layers.Dense(64, input_dim=latent_dim, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.Dense(data_dim, activation='sigmoid', kernel_initializer='he_normal'),
+    ])
+    # model.summary()
+
+    noise = keras.layers.Input(shape=(latent_dim,))
+    data = model(noise)
+
+    return keras.models.Model(noise, data)
 
 
-class GAN:
-    def __init__(self, train_df, noise_dim, gen_lr, dis_lr, batch_size, epochs, seed=1234):
-        self.data_dim = train_df.shape[1]
-        self.noise_dim = noise_dim
-        self.batch_size = batch_size
-        self.epochs = epochs
-        self.max_number_of_steps = epochs
+def build_discriminator(data_dim):
+    model = keras.Sequential([
+        keras.layers.Dense(64, input_dim=data_dim, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.BatchNormalization(),
+        keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
+        keras.layers.Dense(1, activation='sigmoid', kernel_initializer='he_normal'),
+    ])
+    # model.summary()
 
-        # tf.compat.v1.set_random_seed(seed)
-        # tf.compat.v1.reset_default_graph()
+    data = keras.layers.Input(shape=(data_dim,))
+    validity = model(data)
 
-        self.train_dataset = self.convert_to_dataset(train_df, batch_size)
-        self.gan_model = self.build_gan_model()
-        self.gan_loss = self.build_gan_loss(self.gan_model)
-        self.gan_ops = self.build_train_optimizer(self.gan_model, self.gan_loss, gen_lr, dis_lr)
+    return keras.models.Model(data, validity)
 
-    def build_gan_model(self):
-        noise = tf.random.normal([self.batch_size, self.noise_dim])
 
-        gan_model = tfgan.gan_model(
-            generator_fn=self.generator,
-            discriminator_fn=self.discriminator,
-            real_data=self.train_dataset,
-            generator_inputs=noise)
+def build_gan_network(gen_lr, dis_lr, beta_1, data_dim, latent_dim):
+    generator_optimizer = keras.optimizers.Adam(lr=gen_lr)
+    discriminator_optimizer = keras.optimizers.Adam(lr=dis_lr)
 
-        return gan_model
+    # Build and compile the discriminator
+    discriminator = build_discriminator(data_dim)
+    discriminator.compile(loss='binary_crossentropy', optimizer=discriminator_optimizer, metrics=['accuracy'])
 
-    @staticmethod
-    def build_gan_loss(gan_model):
-        # Example of classical loss function.
-        # vanilla_gan_loss = tfgan.gan_loss(
-        #    gan_model,
-        #    generator_loss_fn=tfgan.losses.minimax_generator_loss,
-        #    discriminator_loss_fn=tfgan.losses.minimax_discriminator_loss)
-        # return vanilla_gan_loss
+    # Build the generator
+    generator = build_generator(data_dim, latent_dim)
 
-        # Wasserstein loss (https://arxiv.org/abs/1701.07875) with the
-        # gradient penalty from the improved Wasserstein loss paper
-        # (https://arxiv.org/abs/1704.00028).
-        improved_wgan_loss = tfgan.gan_loss(
-            gan_model,
-            generator_loss_fn=tfgan.losses.wasserstein_generator_loss,
-            discriminator_loss_fn=tfgan.losses.wasserstein_discriminator_loss,
-            gradient_penalty_weight=1.0)
+    # The generator takes noise as input and generates data
+    z = keras.layers.Input(shape=(latent_dim,))
+    data = generator(z)
 
-        return improved_wgan_loss
+    # For the combined model we will only train the generator
+    discriminator.trainable = False
 
-    @staticmethod
-    def build_train_optimizer(gan_model, gan_loss, gen_lr, dis_lr):
-        generator_optimizer = tf.compat.v1.train.AdamOptimizer(gen_lr, beta1=0.5)
-        discriminator_optimizer = tf.compat.v1.train.AdamOptimizer(dis_lr, beta1=0.5)
-        gan_train_ops = tfgan.gan_train_ops(
-            gan_model,
-            gan_loss,
-            generator_optimizer=generator_optimizer,
-            discriminator_optimizer=discriminator_optimizer,
-            summarize_gradients=True,)
+    # The discriminator takes generated images as input and determines validity
+    validity = discriminator(data)
 
-        return gan_train_ops
+    # The combined model  (stacked generator and discriminator)
+    # Trains the generator to fool the discriminator
+    combined = keras.models.Model(z, validity)
+    combined.compile(loss='binary_crossentropy', optimizer=generator_optimizer)
 
-    def train(self):
-        tfgan.gan_train(
-            self.gan_ops,
-            hooks=[tf.train.StopAtStepHook(num_steps=self.max_number_of_steps)],
-            logdir='./output/tf')
-        # g_d_updates_per_step = tfgan.GANTrainSteps(1, 2)  # do 1 gen step, then 2 disc steps.
-        # train_step_fn = tfgan.get_sequential_train_steps(g_d_updates_per_step)
-        # global_step = tf.train.get_or_create_global_step()
-        #
-        # n_batches = 1501
-        # with tf.train.SingularMonitoredSession() as sess:
-        #     for i in range(n_batches):
-        #         train_step_fn(sess, self.gan_ops, global_step, train_step_kwargs={})
-        #         # if i % 100 == 0:
-        #         #     digits_np = sess.run([tfgan.eval(self.predict(100))])
-        #         #     print(digits_np)
+    return generator, discriminator, combined
 
-    def predict(self, gen_size):
-        with tf.Session().as_default():
-            with tf.variable_scope('Generator', reuse=True):
-                noise = tf.random.normal([gen_size, self.noise_dim])
-                gen_data = self.gan_model.generator_fn(noise)
-            return gen_data.eval()
 
-    def convert_to_dataset(self, train_df, batch_size):
-        dataset = tf.data.Dataset.from_tensor_slices(train_df.astype('float32').values)
-        batched_dataset = self.dataset_to_stream(dataset, batch_size)
-        return batched_dataset
+def train(df, epochs, batch_size, latent_dim, generator, discriminator, combined):
+    # Adversarial ground truths
+    valid = np.ones((batch_size, 1))
+    fake = np.zeros((batch_size, 1))
 
-    @staticmethod
-    def dataset_to_stream(inp, batch_size):
-        with tf.device('/cpu:0'):
-            batched = inp.batch(batch_size, drop_remainder=True)
-            data_feeder = tf.compat.v1.data.make_one_shot_iterator(batched.repeat()).get_next()
-        return data_feeder
+    last_idx = (df.shape[0] // batch_size) * batch_size
+    for epoch in range(epochs):
+        for idx in range(0, df.shape[0], batch_size):
+            if idx == last_idx:
+                continue
 
-    def generator(self, inputs):
-        model = keras.Sequential([
-            keras.layers.Dense(64, input_dim=self.noise_dim, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.Dense(self.data_dim, activation='sigmoid', kernel_initializer='he_normal'),
-        ])(inputs)
-        return model
+            ## Train Discriminator
+            idx_list = df.index[idx: idx + batch_size]
+            selected_df = df.loc[idx_list]
 
-    def discriminator(self, inputs, unused_conditioning):
-        model = keras.Sequential([
-            keras.layers.Dense(64, input_dim=self.data_dim, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.Dense(64, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.Dense(32, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.Dense(16, kernel_initializer='he_normal', activation='relu'),
-            keras.layers.Dense(1, activation='sigmoid', kernel_initializer='he_normal'),
-        ])(inputs)
-        return model
+            # Generate a batch of new data
+            noise = np.random.normal(0, 1, (batch_size, latent_dim))
+            generated_data = generator.predict(noise)
+
+            # Train the discriminator
+            discriminator.trainable = True
+            d_loss_real = discriminator.train_on_batch(selected_df, valid)
+            d_loss_fake = discriminator.train_on_batch(generated_data, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            ## Train Generator
+            # Train the generator (to have the discriminator label samples as valid)
+            noise = np.random.normal(0, 1, (batch_size, latent_dim))
+            discriminator.trainable = False
+            g_loss = combined.train_on_batch(noise, valid)
+
+            # Plot the progress
+            if (idx % (batch_size * 100)) == 0 or idx == last_idx - batch_size:
+                print("%d/%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" %
+                      (epoch, idx, d_loss[0], 100 * d_loss[1], g_loss))
