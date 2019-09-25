fe5f598536769f1d5d6b3cc44dc7ec2feabc796b
Jaehwan Park <turnaround0@gmail.com>
2019-09-25 10:33:01 +0900

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -13,6 +13,14 @@ def num_class(df, predict_attr, treatment_attr):
     return tr.shape[0], tn.shape[0], cr.shape[0], cn.shape[0]
 
 
+def split_class(df, predict_attr, treatment_attr):
+    tr = df[(df[predict_attr] != 0) & (df[treatment_attr] == 1)]  # Responders in Treatment group
+    tn = df[(df[predict_attr] == 0) & (df[treatment_attr] == 1)]  # Non-responders in Treatment group
+    cr = df[(df[predict_attr] != 0) & (df[treatment_attr] == 0)]  # Responders in Control group
+    cn = df[(df[predict_attr] == 0) & (df[treatment_attr] == 0)]  # Non-responders in Control group
+    return tr, tn, cr, cn
+
+
 def save_json(name, data):
     with open('output/' + name + '.json', 'w') as f:
         json.dump(data, f)
