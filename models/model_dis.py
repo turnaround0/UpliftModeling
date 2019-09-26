f41f8c4ce3fc5eeecc2be095bab4c2ed835a9fe9
Jaehwan Park <turnaround0@gmail.com>
2019-09-26 16:44:17 +0900

    Update model_dis.py

--- models/model_dis.py
+++ models/model_dis.py
@@ -13,14 +13,15 @@ def predict(obj, newdata, **kwargs):
     for t, y in [(1, 1), (1, 0), (0, 1), (0, 0)]:
         df['T'] = t
         df['Y'] = y
+        discriminator.trainable = False
         pred = discriminator.predict(df)
         pred_list.append(pd.DataFrame([val[0] for val in pred]))
 
     pred_df = pd.concat(pred_list, axis=1)
     pred_df.columns = ['tr', 'tn', 'cr', 'cn']
 
-    pred_treat = pred_df['tr'].div(pred_df['tr'] + pred_df['tn']).fillna(0)
-    pred_control = pred_df['cr'].div(pred_df['cr'] + pred_df['cn']).fillna(0)
+    pred_treat = pred_df['tr'].div(pred_df['tr'] + pred_df['tn'] + 1e-8)
+    pred_control = pred_df['cr'].div(pred_df['cr'] + pred_df['cn'] + 1e-8)
 
     pred_df = pd.DataFrame({
         "pr_y1_t1": pred_treat,
