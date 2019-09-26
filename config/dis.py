decde8af1afaad6701b45d63b0b32bdc556da895
Jaehwan Park <turnaround0@gmail.com>
2019-09-26 11:36:55 +0900

    Update dis.py

--- config/dis.py
+++ config/dis.py
@@ -1,7 +1,7 @@
 from models import model_dis
 from over import gan
 
-params_gan_hillstrom = {
+params_tfgan_hillstrom = {
     'gen_lr': 1e-4,
     'dis_lr': 7e-6,
     'batch_size': 256,
@@ -11,11 +11,11 @@ params_gan_hillstrom = {
     'minor_ratio': 0,
     'loss_type': 'vanilla',
 }
-params_gan_criteo = {
+params_tfgan_criteo = {
     'gen_lr': 1e-4,
     'dis_lr': 3e-5,
     'batch_size': 64,
-    'epochs': 100,
+    'epochs': 200,
     'noise_size': 64,
     'major_multiple': 0,
     'minor_ratio': 0,
@@ -27,12 +27,12 @@ config = {
         'hillstrom': {
             'dta': {},
             'dis': {'model': model_dis, 'over_sampling': gan.over_sampling,
-                    'params_over': params_gan_hillstrom, 'params': {}},
+                    'params_over': params_tfgan_hillstrom, 'params': {}},
         },
         'criteo': {
             # 'dta': {},
             'dis': {'model': model_dis, 'over_sampling': gan.over_sampling,
-                    'params_over': params_gan_criteo, 'params': {}},
+                    'params_over': params_tfgan_criteo, 'params': {}},
         },
     },
     'wrapper': False,
